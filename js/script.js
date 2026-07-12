d690e6e934c6d1f053ad1b4c263ac0e70bfba467
sarahaja13 <20250410040@uniku.ac.id>
2026-07-12 20:04:13 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -83,19 +83,3 @@ musicBtn.addEventListener("click",()=>{
     }
 
 });
-
-const navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll", () => {
-
-    if(window.scrollY > 40){
-
-        navbar.classList.add("scrolled");
-
-    }else{
-
-        navbar.classList.remove("scrolled");
-
-    }
-
-});
